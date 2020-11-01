1e8875aa2e0fa23b18c3eb0aec93a3249c334825
Tanzi876 <tanyaleelawrence@gmail.com>
2020-11-01 12:08:25 -0500

    Edied Code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+
+let button=document.getElementsByClassName('newsletter btn')
+button.onclick=function(){
+    let message=document.getElementsByClassName('message')
+    let email= document.getElementById(email).value
+    
+    if(email==""){
+        message.innerHTML="Please enter a valid email address" 
+        console.log("incorrect")
+    }else{
+        message.innerHTML="Thank you!"+email+
+        "has been to our mailing list"
+        console.log("correct")
+    }
+
+
+}
